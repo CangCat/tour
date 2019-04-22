b7c9b331e2ff181101debbc3f7be7cbdc5f04bab
JLB <JLB@DESKTOP-MHJLC7U>
2019-04-22 15:20:17 +0800

    mr: 2019/4/22 .gitignore设置忽略 cn.team.mr.controller包 newcontroller测试文件

---
+++ src/main/java/cn/team/mr/controller/newcontroller.java
@@ -0,0 +1,14 @@
+package cn.team.mr.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class newcontroller {
+	
+	@RequestMapping("hi")
+	public String hal(){
+		return "halou";
+	}
+	
+}
