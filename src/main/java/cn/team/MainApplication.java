d783454f92a7b62033de70251bf8f55396f47775
cangcat <38480541@qq.com>
2019-04-22 12:37:10 +0800

    Initial commit 初次上传

---
+++ src/main/java/cn/team/MainApplication.java
@@ -0,0 +1,22 @@
+package cn.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author CangCat
+ * @date 2019/4/22 12:15
+ */
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
+
+}
