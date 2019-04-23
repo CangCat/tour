a66d50e435a4093f14b6c21b849ab76d5951e4c9
cangcat <38480541@qq.com>
2019-04-23 10:58:56 +0800

    cangcat  2019/4/23  添加了JSTL

--- src/main/java/cn/team/controller/cangcat/ScenicController.java
+++ src/main/java/cn/team/controller/cangcat/ScenicController.java
@@ -2,6 +2,7 @@ package cn.team.controller.cangcat;
 
 import cn.team.service.cangcat.ScenicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @author CangCat
  * @date 2019/4/22 18:45
  */
-@RestController
+@Controller
 @RequestMapping("scenic")
 public class ScenicController {
 
     @Autowired
     ScenicService scenicService;
 
-    @RequestMapping("toCourtTravel")
-    public String toCourtTravel() {
-        return "";
+    @RequestMapping("toScenicList")
+    public String toScenicList() {
+        return "/WEB-INF/admin/index/scenic-list.jsp";
     }
 
 
