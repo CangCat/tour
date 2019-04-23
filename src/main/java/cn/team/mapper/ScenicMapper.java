524bcf758615b19019114e23251d282a550571b3
cangcat <38480541@qq.com>
2019-04-23 12:13:09 +0800

    cangcat  2019/4/23

---
+++ src/main/java/cn/team/mapper/ScenicMapper.java
@@ -0,0 +1,13 @@
+package cn.team.mapper;
+
+import cn.team.entity.Scenic;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author CangCat
+ * @date 2019/4/23 11:30
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface ScenicMapper extends Mapper<Scenic> {
+
+}
